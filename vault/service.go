3490ea84f353c7bd56c40e64d6b811c8f2aa0498
FP-OD1133 <s403160087@gmail.com>
2022-08-05 15:39:02 +0800

    constructors of req & res. decode & encode  func.

--- vault/service.go
+++ vault/service.go
@@ -24,3 +24,43 @@ func (vaultService) Validate(ctx context.Context, password string, hash string) 
 	}
 	return true, nil
 }
+type hashReq struct {
+	Password string `json:"password"`
+}
+
+func decodeHashReq(ctx context.Context, r *http.Request) (interface{}, error) {
+	var req hashReq
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+type hashRes struct {
+	Hash string `json:"hash"`
+	Err  string `json:"err,omitempty"`
+}
+
+type validateReq struct {
+	Password string `json:"password"`
+	Hash     string `json:"hash"`
+}
+
+type validateRes struct {
+	Valid bool   `json:"valid"`
+	Err   string `json:"err,omitempty"`
+}
+
+func decodeValidateReq(ctx context.Context, r *http.Request) (interface{}, error) {
+	var req validateReq
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func encodeRes(ctx context.Context, w http.ResponseWriter, res interface{}) error {
+	return json.NewEncoder(w).Encode(res)
+}
