7791fcd8345becafa1f303d0d9aba47f362ef7e2
FP-OD1133 <s403160087@gmail.com>
2022-08-05 15:38:12 +0800

    implement service interface with type vaultService(& hash & validate func).

--- vault/service.go
+++ vault/service.go
@@ -8,3 +8,19 @@ type Service interface {
 }
 
 type vaultService struct{}
+
+func (vaultService) Hash(ctx context.Context, password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func (vaultService) Validate(ctx context.Context, password string, hash string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return false, nil
+	}
+	return true, nil
+}
