d0a0911cb015b7434cfe94adaf93e259a84fe40e
FP-OD1133 <s403160087@gmail.com>
2022-08-05 13:39:42 +0800

    init

---
+++ vault/service.go
@@ -0,0 +1,10 @@
+package vault
+
+import "context"
+
+type Service interface {
+	Hash(ctx context.Context, password string) (string, error)
+	Validate(ctx context.Context, password, hash string) (bool, error)
+}
+
+type vaultService struct{}
