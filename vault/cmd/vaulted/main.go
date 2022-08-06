da103eca2bbb4f86721cee41c57a92575c7390f8
FP-OD1133 <s403160087@gmail.com>
2022-08-06 19:04:55 +0800

    add main

---
+++ vault/cmd/vaulted/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc/vault"
+	pb "grpc/vault/pb"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	var (
+		httpAddr = flag.String("http", ":8080", "http listen addr")
+		gRPCAddr = flag.String("grpc", ":8081", "gRPC listen addr")
+	)
+	flag.Parse()
+	ctx := context.Background()
+	srv := vault.NewService()
+	errChan := make(chan error)
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	hashEndpoint := vault.MakeHashEndpoint(srv)
+	validateEndpoint := vault.MakeValidateEndpoint(srv)
+	endpoints := vault.Endpoints{
+		HashEndpoint:     hashEndpoint,
+		ValidateEndpoint: validateEndpoint,
+	}
+
+	// HTTP transport
+	go func() {
+		log.Println("http:", *httpAddr)
+		handler := vault.NewHTTPServer(ctx, endpoints)
+		errChan <- http.ListenAndServe(*httpAddr, handler)
+	}()
+
+	// a lower-level TCP network listener to serve the gRPC server over it
+	go func() {
+		listener, err := net.Listen("tcp", *gRPCAddr)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		log.Println("grpc:", *gRPCAddr)
+		handler := vault.NewGRPCServer(ctx, endpoints)
+		gRPCServer := grpc.NewServer()
+		pb.RegisterVaultServer(gRPCServer, handler)
+		errChan <- gRPCServer.Serve(listener)
+	}()
+
+	log.Fatalln(<-errChan)
+
+}
